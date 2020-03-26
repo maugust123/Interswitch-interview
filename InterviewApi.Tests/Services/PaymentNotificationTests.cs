b9417c37aae2f1d5f82e9e69c50daea9dc9c9a23
Morga Kamoga <maugust123@gmail.com>
2020-03-26 05:24:53 +0300

    Code cleanup

--- InterviewApi.Tests/Services/PaymentNotificationTests.cs
+++ InterviewApi.Tests/Services/PaymentNotificationTests.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using InterviewApi.BusinessLogic.Models;
 using InterviewApi.BusinessLogic.Services.HttpWraper;
